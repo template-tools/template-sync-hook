ffedea9a6b8ed4e1967dec531d26a610d69d5e47
Markus Felten <Markus.Felten@gmx.de>
2017-12-04 22:15:07 +0100

    chore(rollup): initial config

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import pkg from './package.json';
+
+const external = [];
+
+export default [
+  {
+    output: {
+      file: pkg.bin['npm-template-sync-github-hook'],
+      format: 'cjs',
+      banner: '#!/usr/bin/env node'
+    },
+    plugins: [nodeResolve(), commonjs()],
+    external,
+    input: 'src/npm-template-sync-github-hook.js'
+  }
+];
