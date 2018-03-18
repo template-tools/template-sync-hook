aa2dc8e36fe7835859edda544d29f2163822e49b
Markus Felten <Markus.Felten@gmx.de>
2018-03-18 22:29:20 +0100

    test: trying to fake github request

--- tests/github-hook-test.js
+++ tests/github-hook-test.js
@@ -2,9 +2,19 @@ import test from 'ava';
 import {} from '../src/npm-template-sync-github-hook';
 
 const got = require('got');
+const { signer } = require('x-hub-signature');
 
 test('request', async t => {
-  const response = await got.post(`http://localhost:3000/webhook`);
+  const sign = signer({ algorithm: 'sha1', secret: 'dfgkjd&dfh' });
+  const signature = sign(new Buffer('random-signature-body'));
+
+  const response = await got.post(`http://localhost:3000/webhook`, {
+    headers: {
+      'x-hub-signature': signature,
+      'x-github-event': 'push',
+      'x-github-delivery': '77'
+    }
+  });
 
   console.log(response.body);
 
