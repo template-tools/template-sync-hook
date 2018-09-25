ed0310de8a992edd02c4b95f07bc12c804981bd8
Markus Felten <Markus.Felten@gmx.de>
2018-09-25 15:49:41 +0200

    fix(rollup): make use of rollup-plugin-cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
-import cleanup from 'rollup-plugin-cleanup';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
-import pkg from './package.json';
+import cleanup from "rollup-plugin-cleanup";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import pkg from "./package.json";
 
-const external = ['npm-template-sync', 'github-repository-provider'];
+const external = ["npm-template-sync", "github-repository-provider"];
 
 export default [
   {
     output: {
-      file: pkg.bin['npm-template-sync-github-hook'],
-      format: 'cjs',
-      banner: '#!/usr/bin/env node',
+      file: pkg.bin["npm-template-sync-github-hook"],
+      format: "cjs",
+      banner: "#!/usr/bin/env node",
       interop: false
     },
-    plugins: [resolve(), commonjs(), executable()],
+    plugins: [resolve(), commonjs(), cleanup(), executable()],
     external,
-    input: 'src/npm-template-sync-github-hook.js'
+    input: "src/npm-template-sync-github-hook.js"
   }
 ];
