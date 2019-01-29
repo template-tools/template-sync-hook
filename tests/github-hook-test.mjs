1b7ecedac5092afd0b651aac467cf7ee9df5b525
Markus Felten <Markus.Felten@gmx.de>
2019-01-29 21:36:13 +0100

    test: *.js -> *.mjs

--- tests/github-hook-test.mjs
+++ tests/github-hook-test.mjs
@@ -1,28 +1,28 @@
 import test from "ava";
 import {} from "../src/npm-template-sync-github-hook";
 
-const got = require("got");
-const { signer } = require("x-hub-signature");
+import got from "got";
+import signer from "x-hub-signature/src/signer";
+
+//const { signer } = require("x-hub-signature");
 
 test("request", async t => {
   const secret = "aSecret";
+  const port = "3100";
 
   process.env.WEBHOOK_SECRET = secret;
-  process.env.PORT = "3100";
+  process.env.PORT = port;
 
   const sign = signer({ algorithm: "sha1", secret });
   const signature = sign(new Buffer("random-signature-body"));
 
-  const response = await got.post(
-    `http://localhost:${process.env.PORT}/webhook`,
-    {
-      headers: {
-        "x-hub-signature": signature,
-        "x-github-event": "push",
-        "x-github-delivery": "77"
-      }
+  const response = await got.post(`http://localhost:${port}/webhook`, {
+    headers: {
+      "x-hub-signature": signature,
+      "x-github-event": "push",
+      "x-github-delivery": "77"
     }
-  );
+  });
 
   console.log(response.body);
 
