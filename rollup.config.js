3bf16155e6f49469c108be56477655046fc07d1d
Markus Felten <Markus.Felten@gmx.de>
2019-11-14 12:44:24 +0100

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -17,32 +17,45 @@ const external = [
   ];
   
 const extensions = ["js", "mjs", "jsx", "tag"];
+const plugins = [
+  commonjs(),
+  resolve(),
+  json({
+ //   include: "package.json",
+    preferConst: true,
+    compact: true
+  }),
+  cleanup({
+    extensions
+  })
+];
 
-export default
-  Object.keys(pkg.bin || {}).map(name => {
-    return {
-      input: `src/${name}-cli.mjs`,
-      output: {
-        file: pkg.bin[name],
-        format: "cjs",
-        banner:
-          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
-        interop: false,
-        externalLiveBindings: false
-      },
-      plugins: [
-        commonjs(),
-        resolve(),
-        json({
-          //include: "package.json",
-          preferConst: true,
-          compact: true
-        }),
-        cleanup({
-          extensions
-        }),
-        executable()
-      ],
-      external
-    };
+const config = Object.keys(pkg.bin || {}).map(name => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      banner:
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+      file: pkg.bin[name]
+    }
+  };
+});
+
+if (pkg.module !== undefined && pkg.main !== undefined) {
+  config.push({
+    input: pkg.module,
+    output: {
+      file: pkg.main
+    }
   });
+}
+export default config.map(c => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
+    format: "cjs",
+    ...c.output
+  };
+  return { plugins, external, ...c };
+});
