1ad6e6a696bb5a1f0a1a89eb3a84b2421c6b6c86
Markus Felten <Markus.Felten@gmx.de>
2019-11-14 12:15:38 +0100

    chore(rollup): sync with template

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,48 @@
-import builtins from "builtin-modules";
-import cleanup from "rollup-plugin-cleanup";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import json from "rollup-plugin-json";
 import executable from "rollup-plugin-executable";
+import json from "@rollup/plugin-json";
+import cleanup from "rollup-plugin-cleanup";
+import builtins from "builtin-modules";
 import pkg from "./package.json";
 
 const external = [
-  ...builtins,
-  "universal-user-agent",
-  "@octokit/rest",
-  "node-fetch",
-  "sd-daemon",
-  "koa",
-  "koa-better-router"
-];
+    ...builtins,
+    "universal-user-agent",
+    "@octokit/rest",
+    "node-fetch",
+    "sd-daemon",
+    "koa",
+    "koa-better-router"
+  ];
+  
+const extensions = ["js", "mjs", "jsx", "tag"];
 
-export default {
-  output: {
-    file: pkg.bin["npm-template-sync-github-hook"],
-    format: "cjs",
-    banner:
-      '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"',
-    interop: false,
-    externalLiveBindings: false
-  },
-  plugins: [
-    commonjs(),
-    resolve(),
-    json({
-      preferConst: true,
-      compact: true
-    }),
-    cleanup({
-      extensions: ['js','mjs','jsx','tag']
-    }),
-    executable()
-  ],
-  external,
-  input: pkg.module
-};
+export default
+  Object.keys(pkg.bin || {}).map(name => {
+    return {
+      input: `src/${name}-cli.mjs`,
+      output: {
+        file: pkg.bin[name],
+        format: "cjs",
+        banner:
+          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+        interop: false,
+        externalLiveBindings: false
+      },
+      plugins: [
+        commonjs(),
+        resolve(),
+        json({
+          //include: "package.json",
+          preferConst: true,
+          compact: true
+        }),
+        cleanup({
+          extensions
+        }),
+        executable()
+      ],
+      external
+    };
+  });
