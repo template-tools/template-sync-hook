48a9960d19ffd0d226dceba79d19e1c9d99ae623
Markus Felten <Markus.Felten@db.com>
2020-05-16 17:45:02 +0200

    Revert "fix(pacman): systemct daemon-reload is done automatically"
    This reverts commit fb7f463e8ec2b1fa62d8a86115ad4244973da6f1.

--- pacman/hooks.sh
+++ pacman/hooks.sh
@@ -1,5 +1,6 @@
 
 post_install() {
+	systemctl daemon-reload
 	systemctl enable {{name}}
 	systemctl enable {{name}}.socket
 	systemctl start {{name}}.socket
@@ -12,6 +13,7 @@ pre_upgrade() {
 }
 
 post_upgrade() {
+	systemctl daemon-reload
 	systemctl start {{name}}.socket
 	systemctl is-enabled nginx 2>&1 >/dev/null && systemctl -q try-reload-or-restart nginx
 }
@@ -24,5 +26,6 @@ pre_remove() {
 }
 
 post_remove() {
+	systemctl daemon-reload
 	systemctl is-enabled nginx 2>&1 >/dev/null && systemctl -q try-reload-or-restart nginx
 }
